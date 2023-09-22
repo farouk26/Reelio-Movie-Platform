84c37ca8ebff2877aed8ccee23aa9157e8a0544e
Farouk <faroukwebdesign@gmail.com>
2023-09-22 23:32:48 +0100

    fix(card height): fix card height and responsiveness

--- src/components/Cards/Movie-Card.jsx
+++ src/components/Cards/Movie-Card.jsx
@@ -1,27 +1,30 @@
-function MovieCard({ title, release_date, overview, popularity, poster_path }) {
+import { FaStar } from "react-icons/fa"
+
+function MovieCard({ title, release_date, vote_average, poster_path }) {
   return (
-    <>
-      <div className="card w-full shadow-xl rounded-3xl transform hover:scale-105 transition-transform duration-300 ease-in-out">
-        <figure className="h-64">
-          <img
-            src={`https://image.tmdb.org/t/p/w500${poster_path}`}
-            alt="Movie"
-            className="h-full w-full object-cover"
-          />
-        </figure>
-        <div className="card-body h-40 rounded-b-2xl grid cols-3 p-0 bg-gradient-to-b from-slate-900 to-gray-900 bg-opacity-20">
-          <h2 className="card-title col-span-4 ml-2 mt-3 text-1xl font-bold p-0  text-white">
-            {title}
-          </h2>
-          <p className="col-span-2 ml-2 mt-2 text-sm self-start p-0 text-start text-white">
-            <strong>Popularity {popularity}</strong>
-          </p>
-          <p className="col-span-2 ml-3 mt-3 mr-3 mb-2 pb-10 self-start justify-center text-end text-sm text-white">
-            <strong>{release_date}</strong>
-          </p>
+    <div className="card bg-slate-800 shadow-xl " style={{ height: "300px" }}>
+      <figure className="h-56">
+        <img
+          src={`https://image.tmdb.org/t/p/w500/` + poster_path}
+          alt="Movie Poster"
+          className="w-full h-full object-cover"
+        />
+      </figure>
+      <div className="card-body p-0 grid grid-cols-4 w-full items-center">
+        <div className="col-span-4 pl-1" style={{ maxHeight: "2.5em" }}>
+          <h2 className="card-title text-base">{title}</h2>
         </div>
+        <p className="col-span-2 pl-2 pb-2 text-sm flex justify-start gap-2 items-center">
+          <span className="text-yellow-200">
+            <FaStar />
+          </span>
+          {vote_average}
+        </p>
+        <p className="col-span-2 pr-2 pb-2 text-sm justify-self-end">
+          {release_date}
+        </p>
       </div>
-    </>
+    </div>
   )
 }
 
